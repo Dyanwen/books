89dbc3cb6748b5d77ff2567252357c3cea85e509
deng.yanwen <1264007445@qq.com>
2021-01-09 23:08:56 +0800

    test010923

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ git push
 echo "提交完成"
 
 git clone -b gh-pages https://github.com/Dyanwen/books.git book_end
-rm -rf ./book_end/*
+# rm -rf ./book_end/*
 
 echo "代码clone成功"
 
@@ -15,9 +15,9 @@ gitbook build ./ ./book_end
 
 echo "构建成功"
 
-cd ./book_end
-git add .
-git commit -m $1 
-git push
-# cd -
-echo "执行完毕"
+# cd ./book_end
+# git add .
+# git commit -m $1 
+# git push
+# # cd -
+# echo "执行完毕"
