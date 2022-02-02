477d110887d7c725a5519a27f9e3c24e23222289
Tianmo <1025568329@qq.com>
2022-02-02 21:03:51 +0800

    update;for-of

--- JS/for-of.js
+++ JS/for-of.js
@@ -3,3 +3,10 @@ for(var val,ret,it=a[Symbol.iterator]();(ret=it.next())&&!ret.done;){
     val = ret.value;
     console.log(val);
 }
+let n = a[Symbol.iterator]();
+console.log(n.next());
+console.log(n.next());
+console.log(n.next());
+console.log(n.next());
+console.log(n.next());
+console.log(n.next());
